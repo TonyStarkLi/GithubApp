4f3c7bbf186e325a383cd1e0c06b72bc8617f837
Botao Li <1114338553@qq.com>
2020-02-16 20:58:56 -0500

    Initial commit

---
+++ GithubFollowers/GithubFollowers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by BOTAO LI on 2/16/20.
+//  Copyright © 2020 BOTAO LI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+

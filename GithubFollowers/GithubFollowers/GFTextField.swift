f6b972776737619804bf42ca476ad13223be981a
Botao Li <1114338553@qq.com>
2020-02-18 20:48:56 -0500

    Custom Views
    Custom Reusable objects
    Subclassing
    Dynamic Type
    SemanticUI App
    Custom Initializers

---
+++ GithubFollowers/GithubFollowers/GFTextField.swift
@@ -0,0 +1,42 @@
+//
+//  GFTextField.swift
+//  GithubFollowers
+//
+//  Created by BOTAO LI on 2/18/20.
+//  Copyright © 2020 BOTAO LI. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        
+        /* Support long username but with minimum size of 12 */
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}

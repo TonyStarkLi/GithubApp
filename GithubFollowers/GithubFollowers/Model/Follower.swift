67987a98013a6c62c455e11d628dc8a34da7b23e
Botao Li <1114338553@qq.com>
2020-03-21 16:44:18 -0400

    Project Orgnization & Planning API call

---
+++ GithubFollowers/GithubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by BOTAO LI on 3/21/20.
+//  Copyright © 2020 BOTAO LI. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
